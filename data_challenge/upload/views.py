735f3de3c1d266488165b8a00f144c09959c2874
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 13:59:13 +0100

    Update views.py
    Adding to the post method, the file should now be read by pandas and all the glucose levels should get stored into the database.

--- data_challenge/upload/views.py
+++ data_challenge/upload/views.py
@@ -1,6 +1,11 @@
+import os
+import pandas
+from data_challenge.settings import MEDIA_ROOT
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from upload.forms import UploadForm
+from upload.models import GlucoseLevels
+from datetime import datetime
 
 
 class UploadView(TemplateView):
@@ -15,6 +20,24 @@ class UploadView(TemplateView):
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            file = request.FILES['file']
+            filename = file.name
+            df = pandas.read_csv(os.path.join(MEDIA_ROOT, 'uploads', filename), skiprows=2).fillna(0)
+            user_id = filename[:-4]
+            model_list = list()
+            for i in range(df.shape[0]):
+                row = df[i:i + 1]
+                device = row['Gerät'].item()
+                serial_number = row['Seriennummer'].item()
+                # Reformat time to datetime field.
+                timestamp = datetime.strptime(row['Gerätezeitstempel'].item(), '%d-%m-%Y  %H:%M')
+                timestamp = str(timestamp.strftime('%Y-%m-%d %H:%M'))
+                glucose_value_history = row['Aufzeichnungstyp'].item()
+                glucose_levels = row['Glukosewert-Verlauf mg/dL'].item()
+                model = GlucoseLevels(user_id=user_id, device=device, serial_number=serial_number, timestamp=timestamp,
+                                      glucose_value_history=glucose_value_history, glucose_levels=glucose_levels)
+                model_list.append(model)
+            GlucoseLevels.objects.bulk_create(model_list)
 
         return render(request, 'upload/upload.html', {'form': UploadForm()})
 
