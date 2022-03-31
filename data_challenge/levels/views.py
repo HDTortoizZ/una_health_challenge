db74429d736fc059b5894ce993d6bcfe5f7a8687
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 15:44:43 +0100

    Update views.py
    Create post method for filter home view.

--- data_challenge/levels/views.py
+++ data_challenge/levels/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from levels.forms import filter_form_gen
+from upload.models import GlucoseLevels
+from datetime import datetime
 
 
 filter_form = filter_form_gen()
@@ -10,3 +12,14 @@ class LevelsView(TemplateView):
     def get(self, request):
         form = next(filter_form)
         return render(request, 'levels/levels_home.html', {'form': form})
+
+    def post(self, request):
+        form_class = next(filter_form)
+        form = form_class(request.POST)
+        timestamp = request.POST["timestamp"].replace(" ", "--")
+        sort = request.POST['sort']
+        max_rows = request.POST['max_rows']
+
+        if form.is_valid():
+            return redirect(f'{request.POST["user_id"]}/{timestamp}/{sort}/{max_rows}')
+
