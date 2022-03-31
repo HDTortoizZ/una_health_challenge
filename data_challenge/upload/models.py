205f1bc0e896899a1a4668058f3e21cbd2d63d03
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 13:03:34 +0100

    Update models.py
    Created a model for Glucose levels.

--- data_challenge/upload/models.py
+++ data_challenge/upload/models.py
@@ -6,3 +6,12 @@ class Upload(models.Model):
     # As I understand it, saving files to a directory rather than the database is standard because of performance
     # issues.
     file = models.FileField(upload_to='uploads/')
+
+
+class GlucoseLevels(models.Model):
+    user_id = models.CharField(max_length=100)
+    device = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100)
+    timestamp = models.DateField()
+    glucose_value_history = models.IntegerField()
+    glucose_levels = models.IntegerField()
