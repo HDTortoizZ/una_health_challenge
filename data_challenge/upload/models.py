ef38437c3a251369b7c11ad67418bef87233358f
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 12:39:56 +0100

    Update models.py
    Created a model for uploads.

--- data_challenge/upload/models.py
+++ data_challenge/upload/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Upload(models.Model):
+    # The media root should be set with an uploads directory that this type of model and save files to.
+    # As I understand it, saving files to a directory rather than the database is standard because of performance
+    # issues.
+    file = models.FileField(upload_to='uploads/')
