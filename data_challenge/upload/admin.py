6597323374adc880cb2f2f2e2267baafecd576ff
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 13:58:21 +0100

    Update admin.py
    Added Glucose models to the admin site.

--- data_challenge/upload/admin.py
+++ data_challenge/upload/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from upload.models import Upload
+from upload.models import Upload, GlucoseLevels
 
 
 admin.site.register(Upload)
+admin.site.register(GlucoseLevels)
